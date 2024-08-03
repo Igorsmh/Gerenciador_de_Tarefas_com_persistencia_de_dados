025533817d0d4423591e74186bcf78d270192e1f
Igor <shenriques@ihm.com.br>
2024-08-03 02:14:03 -0300

    add-func_visu_tarefas/menu_opcoes

--- src.py
+++ src.py
@@ -3,18 +3,48 @@ import sqlite3
 
 
 escolha = input(
-    "Escolha uma das opções:\n[1] - Criar uma Tarefa\n[2]\
-    - Visualizar Tarefas\n[3] - Marcar Tarefa como concluída\n\
-    [4] - Excluir Tarefa\n[5] - Sair\n")
+"Escolha uma das opções:\n[1] - Criar uma Tarefa\n\
+[2] - Visualizar Tarefas\n[3] - Marcar Tarefa como concluída\n\
+[4] - Excluir Tarefa\n[5] - Sair\n")
 
 #Criar tarefa
-conexao = sqlite3.conect('Tarefas.db')
-cursor = conexao.cursor()
-nova_tarefa = input("Escreva o nome da Tarefa:")
-execute("INSERT INTO Tarefas VALUES (?, ?, ?)", (1, nova_tarefa, 0))
-conexao.comit()
-#Visualizar tarefas
+def criar_tarefa():
 
-#Marcar tarefa como concluída
+    conexao = sqlite3.connect('tarefas.db')
+    cursor = conexao.cursor()
+    nova_tarefa = input("Escreva o nome da Tarefa:")
+
+    cursor.execute("INSERT INTO Tarefas (nome, concluido) VALUES (?, ?)", (nova_tarefa, 0))
+    conexao.commit()
+
+
+#Visualizar tarefas
+def visualizar_tarefas():
+    conexao = sqlite3.connect('tarefas.db')
+    cursor = conexao.cursor()
+    cursor.execute("SELECT nome FROM Tarefas")
+    print(cursor.fetchall())
+
+
+#Marcar tarefa como concluída1
+def concluir():
+    pass
 #Excluir tarefa
+def excluir():
+    pass
+
 #Sair
+def sair():
+    print("Saindo...")
+    exit()
+
+opcoes = {'1': criar_tarefa, 
+        '2': visualizar_tarefas, 
+        '3': concluir,
+        '4': excluir,
+        '5': sair }
+
+try:
+    opcoes[escolha]()
+except KeyError:
+    print("Opção inválida. Tente novamente.")
