76746945eb671c601a5ab782bb8ea321c6341a98
Igor <shenriques@ihm.com.br>
2024-08-04 01:25:05 -0300

    add_retorno_menu

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from src import *
 
 
 if __name__ == "__main__":
-    menu()
+    
+    while True:
+        criar_base()
+        menu()
+        input("Aperte qualquer tecla para voltar ao menu...")
 
 
 
