57249319c318c938e8263807580f84f67f047d69
Igor <shenriques@ihm.com.br>
2024-08-04 22:49:28 -0300

    add-apresentacao

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from src import *
 
 
 if __name__ == "__main__":
-    
+    apresentacao()
     while True:
         criar_base()
         menu()
-        input("Aperte qualquer tecla para voltar ao menu...")
+        input("Aperte qualquer tecla para voltar ao menu...\n")
 
 
 
